fc9fa59c454c7b38347ab9493d4d1a790428e832
shreejan pandey <neplay7@gmail.com>
2022-10-07 08:29:07 +0545

    Made some changes and trying to add AI

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     string[,] crossOrCircle = new string[3, 3] { { "", "", "" }, { "", "", "" }, { "", "", "" } };
+    TextMeshProUGUI[] texts =new TextMeshProUGUI[9];
 
     private bool turnX = true;
     [SerializeField] TextMeshProUGUI turnText;
@@ -23,13 +21,47 @@ public class GameManager : MonoBehaviour
     int y; int k;
     public void setValue(int x)
     {
+       // if (!turnX) return;
         y = x % 10;
         k = x / 10;
         if (crossOrCircle[k, y] != "") return;       
         crossOrCircle[k, y] = turnX ? "X" : "O";
         turnX = !turnX;
         CheckIfGameEnd();
+      //  if (!turnX)
+         //   AITurn();
     }
+
+    private void AITurn()
+    {
+        //List<List<int>> possibleMoves = new List<List<int>>();
+        //for (int i = 0; i < 3; i++)
+        //{
+
+        //    for (int j = 0; j < 3; j++)
+        //    {
+        //        if (crossOrCircle[i, j] == "")
+        //            possibleMoves.Add(new List<int> { i, j });
+        //    }
+        //}
+        if (crossOrCircle[1, 1] == "")
+        {
+            crossOrCircle[1, 1] = "O";
+            return;
+
+        }
+        if (crossOrCircle[0, 0] == "")
+        {
+            crossOrCircle[0, 0] = "O";
+            return;
+        }
+        if (crossOrCircle[1, 0] == "")
+        {
+            crossOrCircle[1, 1] = "O";
+            return;
+        }
+    }
+
     public void setValue(TextMeshProUGUI x)
     {
        
@@ -44,14 +76,25 @@ public class GameManager : MonoBehaviour
         int allFinish = 0;
         for (int i = 0; i < 3; i++)
         {
-            int count = 0;
+            int sideWays = 0;
+            int down = 0;
             for (int j = 0; j < 3; j++)
             {
+                //Determine if the game has straight line
                 if (crossOrCircle[i, 0] == crossOrCircle[i, j] && crossOrCircle[i, j] != "")
-                    count++;
+                    sideWays++;
+                //Determine if the game has finished without win line
                 if (crossOrCircle[i, j] != "")
                     allFinish++;
-                if (count == 3)
+                if (sideWays == 3)
+                {
+                    GameOver(true);
+                    return;
+                }
+                //Determine if the game has finished without win line
+                if (crossOrCircle[0, i] == crossOrCircle[j, i] && crossOrCircle[j, i] != "")
+                    down++;
+                if (down == 3)
                 {
                     GameOver(true);
                     return;
@@ -63,40 +106,16 @@ public class GameManager : MonoBehaviour
         {
             GameOver(false);
         }
-        for (int i = 0; i < 3; i++)
-        {
-            int count = 0;
-            for (int j = 0; j < 3; j++)
-            {
-                if (crossOrCircle[0, i] == crossOrCircle[j, i] && crossOrCircle[j, i] != "")
-                    count++;
-                if (count == 3)
-                {
-                    GameOver(true);
-                    return;
-                }
-
-            }
-        }
-        {
-            int count = 0;
-            for (int j = 1; j < 3; j++)
-            {
-                if (crossOrCircle[0, 0] == crossOrCircle[j, j] && crossOrCircle[j, j] != "")
-                    count++;
-                if (count == 2)
-                {
-                    GameOver(true);
-                    return;
-                }
-            }
-        }
+      
+      
 
         if (crossOrCircle[0, 2] == crossOrCircle[1, 1] && crossOrCircle[0, 2] == crossOrCircle[2, 0] && crossOrCircle[0, 2] != "")
 
             GameOver(true);
 
+        if (crossOrCircle[0, 0] == crossOrCircle[1, 1] && crossOrCircle[0, 0] == crossOrCircle[2, 2] && crossOrCircle[0, 0] != "")
 
+            GameOver(true);
 
 
     }
